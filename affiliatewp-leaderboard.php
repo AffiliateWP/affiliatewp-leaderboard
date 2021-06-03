e2f0ad1858acc6aa01ac5654244bfd93650f3520
David Beja <david.beja@gmail.com>
2021-06-03 18:02:25 +0100

    issue/24 - Bump the AffiliateWP minimum to 2.6 (#27)
    Co-authored-by: Drew Jaynes <DrewAPicture@users.noreply.github.com>
    
    Fixes #24

--- affiliatewp-leaderboard.php
+++ affiliatewp-leaderboard.php
@@ -54,6 +54,24 @@ class AffiliateWP_LB_Requirements_Check extends AffiliateWP_Requirements_Check {
 	protected $slug = 'affiliatewp-leaderboard';
 
 	/**
+	 * Add-on requirements.
+	 *
+	 * @since 1.1
+	 * @var   array[]
+	 */
+	protected $addon_requirements = array(
+		// AffiliateWP.
+		'affwp' => array(
+			'minimum' => '2.6',
+			'name'    => 'AffiliateWP',
+			'exists'  => true,
+			'current' => false,
+			'checked' => false,
+			'met'     => false,
+		),
+	);
+
+	/**
 	 * Bootstrap everything.
 	 *
 	 * @since 1.1
