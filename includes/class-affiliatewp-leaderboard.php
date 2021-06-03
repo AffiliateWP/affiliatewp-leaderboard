d0c96a4315f13a213475eec912c42bd4452e96cc
dbeja <david.beja@gmail.com>
2021-06-03 12:40:38 +0100

    Add a file header to class-affiliatewp-leaderboard.php. #25

--- includes/class-affiliatewp-leaderboard.php
+++ includes/class-affiliatewp-leaderboard.php
@@ -1,35 +1,22 @@
 <?php
 /**
- * Plugin Name: AffiliateWP - Leaderboard
- * Plugin URI: https://affiliatewp.com/add-ons/official-free/leaderboard/
- * Description: Display an affiliate leaderboard on your website
- * Author: Sandhills Development, LLC
- * Author URI: https://sandhillsdev.com
- * Version: 1.0.2
- * Text Domain: affiliatewp-leaderboard
- * Domain Path: languages
+ * Core: Plugin Bootstrap
  *
- * AffiliateWP is distributed under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * any later version.
- *
- * AffiliateWP is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with AffiliateWP. If not, see <http://www.gnu.org/licenses/>.
- *
- * @package Leaderboard
- * @category Core
- * @author Andrew Munro
- * @version 1.0
+ * @package     AffiliateWP Leaderboard
+ * @subpackage  Core
+ * @copyright   Copyright (c) 2021, Sandhills Development, LLC
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       1.0.0
  */
 
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 
+/**
+ * Main plugin bootstrap.
+ *
+ * @since 1.0.0
+ */
 final class AffiliateWP_Leaderboard {
 
 	/** Singleton *************************************************************/
